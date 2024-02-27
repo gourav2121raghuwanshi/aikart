afbf8380c70a3bbdeb2f1023175413c70b087044
gourav2121raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2024-02-28 00:43:22 +0530

    Backend setUp

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,32 @@
-const express = require('express');
+const express = require('express')
+const cookieParser = require('cookie-parser');
+const authRouter = require('./routes/authRoutes.js');
+const dbConnect=require('./utils/databaseConnect.js');
+const cors = require('cors');
+require('dotenv').config();
+
+dbConnect();
+
 
 const app = express();
+app.use(express.json())
+app.use(cookieParser());
+app.use(cors());
+
+app.listen(process.env.PORT, () => {
+  console.log(`server is running on port ${process.env.PORT}`);
+})
+
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
 
 
 
-
-const port = 3000;
