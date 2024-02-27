aca3118b28af34eb752e9bcbacaca55c83568b08
gourav2121raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2024-02-28 00:47:29 +0530

    one

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,14 @@ require('dotenv').config();
 dbConnect();
 
 
+
 const app = express();
+app.use(express.json())
+app.use(cookieParser());
+app.use(cors());
 
+app.listen(process.env.PORT, () => {
+  console.log(`server is running on port ${process.env.PORT}`);
+})
 
+app.use('/api/auth', authRouter);
