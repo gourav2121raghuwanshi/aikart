5f4d24c1413a22e19518828fc6b6260168664da7
gourav2121raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2024-02-28 00:53:35 +0530

    adding Tailwind to fontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import NavBar from './components/NavBar'
-
-function App() {
-
+export default function App() {
   return (
-    <>
-      <NavBar />
-    </>
+    <h1 className="text-3xl font-bold underline bg-red-600">
+      Hello world!
+    </h1>
   )
 }
-
-export default App
