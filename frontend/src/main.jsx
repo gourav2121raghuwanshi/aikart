13758edce80634f8926e776930f09c7b3f918508
gourav2121raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2024-02-28 01:04:31 +0530

    redux toolkit addition

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,19 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { persistor, store } from './redux/store.js';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-
-const theme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
-});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme} >
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>,
+  <Provider store={store}>
+  <PersistGate loading={null} persistor={persistor}>
+    <App />
+  </PersistGate>
+</Provider>
 )
