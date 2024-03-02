6fc71fe973cc7c77792c97cacae316b59841b899
gourav2121raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2024-03-03 03:20:18 +0530

    Final Changes___

--- frontend/src/components/FAQ.jsx
+++ frontend/src/components/FAQ.jsx
@@ -3,9 +3,9 @@ import { SlArrowDown, SlArrowUp } from "react-icons/sl";
 
 const FAQ = () => {
   const faqData = [
-    { id: 'q1', question: 'How do I contact customer support if I have a question or issue?', answer: ' Lorem Ipsum is simply dummy text of the printing and typesetting industry.' },
-    { id: 'q2', question: 'How do I return the product if it doesn\'t meet my expectations?', answer: ' It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.' },
-    { id: 'q3', question: 'What makes your product stand out from others in the market?', answer: ' Contrary to popular belief, Lorem Ipsum is not simply random text.' },
+    { id: 'q1', question: 'How can I create my own AI application on your platform?', answer: 'Creating your AI application is easy! Simply click  the create  button, choose the desired functionalities such as image-to-text or text-to-text, and follow our intuitive step-by-step guide. Our platform is designed to make the process accessible for users of all skill levels.' },
+    { id: 'q2', question: ' What types of AI applications can I find in the marketplace?', answer: 'Our marketplace hosts a diverse range of AI applications, from image recognition for disease diagnosis to language processing applications. Explore and discover solutions that suit your needs, or be inspired to  innovate.' },
+    { id: 'q3', question: ' Can I monetize the AI applications I create on your platform?', answer: 'No, currently you do not have the option to monetize your AI applications , but our team is working on it.' },
   ];
 
   const [activeAnswer, setActiveAnswer] = useState(null);
