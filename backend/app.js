29fc6a3bf7bb2708d7f54a680ffa8cd368dd9ff3
gourav2121raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2024-03-31 21:57:25 +0530

    changes

--- backend/app.js
+++ backend/app.js
@@ -7,25 +7,25 @@ const aiRoutes = require('./routes/aiRoutes.js');
 const dbConnect = require('./utils/databaseConnect.js');
 //const {upload} = require('./utils/cloudinary.js')
 const upload = require('./utils/fileupload.js')
-const path = require('path');
+// const path = require('path');
 
 const cors = require('cors');
 require('dotenv').config();
 
 dbConnect();
 
-__dirname = path.resolve();
+// __dirname = path.resolve();
 
 
 const app = express();
 app.use(express.json())
 app.use(cookieParser());
-app.use(cors());
-// app.use(cors({
-//   origin: ["https://aikart-s5sk.vercel.app"],
-//   credentials: true,
-//   methods: "GET,PUT,POST,DELETE"
-// }));
+
+app.use(cors({
+  origin: ["https://aikart-s5sk.vercel.app"],
+  credentials: true,
+  methods: "GET,PUT,POST,DELETE"
+}));
 
 app.use(express.static("aikart/backend/public"));
 app.use('/api/auth', authRouter);
@@ -47,10 +47,10 @@ app.get("/", (req, res) => {
   res.send("Hello, world!"); // Sending a simple response
 });
 
-app.use(express.static(path.join(__dirname, '/frontend/dist')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
-})
+// app.use(express.static(path.join(__dirname, '/frontend/dist')));
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+// })
 
 app.listen(process.env.PORT, () => {
   console.log(`server is running on port ${process.env.PORT}`);
