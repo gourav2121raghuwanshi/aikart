5cd730be1cb235c1e10c7d0d2379ea9ad7ed3a43
gourav2121raghuwanshi <124056400+gourav2121raghuwanshi@users.noreply.github.com>
2024-03-31 21:31:40 +0530

    changes

--- backend/app.js
+++ backend/app.js
@@ -1,38 +1,40 @@
 const express = require('express')
 const cookieParser = require('cookie-parser');
 const authRouter = require('./routes/authRoutes.js');
-const userRouter=require("./routes/userRoutes.js");
+const userRouter = require("./routes/userRoutes.js");
 
 const aiRoutes = require('./routes/aiRoutes.js');
-const dbConnect=require('./utils/databaseConnect.js');
+const dbConnect = require('./utils/databaseConnect.js');
 //const {upload} = require('./utils/cloudinary.js')
 const upload = require('./utils/fileupload.js')
-
+const path = require('path');
 
 const cors = require('cors');
 require('dotenv').config();
 
 dbConnect();
 
+__dirname = path.resolve();
 
 
 const app = express();
 app.use(express.json())
 app.use(cookieParser());
-app.use(cors({
-  origin: ["https://aikart-s5sk.vercel.app"],
-  credentials: true,
-  methods: "GET,PUT,POST,DELETE"
-}));
-app.use(express.static("aikart/backend/public"));
+app.use(cors());
+// app.use(cors({
+//   origin: ["https://aikart-s5sk.vercel.app"],
+//   credentials: true,
+//   methods: "GET,PUT,POST,DELETE"
+// }));
 
+app.use(express.static("aikart/backend/public"));
 app.use('/api/auth', authRouter);
 app.use('/api/user', userRouter);
 
 app.use('/ai', aiRoutes);
 
-app.post('/upload', upload.single("file"), (req, res) =>{
-  if(!req.file)return;
+app.post('/upload', upload.single("file"), (req, res) => {
+  if (!req.file) return;
   var destin = req.file.destination;
   destin = "../backend" + destin.substring(1) + req.file.filename;
   req.file["destin"] = destin;
@@ -41,9 +43,15 @@ app.post('/upload', upload.single("file"), (req, res) =>{
 });
 
 app.get("/", (req, res) => {
-  
+
   res.send("Hello, world!"); // Sending a simple response
 });
+
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+})
+
 app.listen(process.env.PORT, () => {
   console.log(`server is running on port ${process.env.PORT}`);
 })
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,13 +6,21 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://aikart-mern.vercel.app',
+        target: 'http://localhost:3000',
         secure: false,
       },
       '/ai': {
-        target: 'https://aikart-mern.vercel.app',
+        target: 'http://localhost:3000',
         secure: false,
       },
+      // '/api': {
+      //   target: 'https://aikart-mern.vercel.app',
+      //   secure: false,
+      // },
+      // '/ai': {
+      //   target: 'https://aikart-mern.vercel.app',
+      //   secure: false,
+      // },
     },
   },
   plugins: [react()],
